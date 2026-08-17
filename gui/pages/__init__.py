43bac4e9f873112af651060dd82a7e762ddf918e
avaxngard <trimansberg81@gmail.com>
2026-08-17 20:43:05 +0400

    Create __init__.py

---
+++ gui/pages/__init__.py
@@ -0,0 +1,25 @@
+# Zapret Launcher - Bypass restrictions
+# Copyright (C) 2026 avaxngard corp
+#
+# This is free software: you can redistribute it and/or modify it
+# under the terms of the GNU GPL v3 or any later version.
+#
+# Distributed WITHOUT ANY WARRANTY.
+
+from .main_page import MainPage
+from .service_page import ServicePage
+from .lists_page import ListsPage
+from .traffic_page import TrafficPage
+from .hosts_page import HostsPage
+from .logs_page import LogsPage
+from .settings_page import SettingsPage
+
+__all__ = [
+    'MainPage',
+    'ServicePage', 
+    'ListsPage',
+    'TrafficPage',
+    'HostsPage',
+    'LogsPage',
+    'SettingsPage'
+]
