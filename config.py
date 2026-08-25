9a573a83fc04afa1b449f012af7562322adf6d0d
avaxngard <trimansberg81@gmail.com>
2026-08-25 16:00:57 +0400

    Build number bump

--- config.py
+++ config.py
@@ -19,9 +19,9 @@ PNG_ICON_PATH = BASE_DIR / "resources" / "icon.png"
 HOSTS_PATH = Path(r"C:\Windows\System32\drivers\etc\hosts")
 
 CURRENT_VERSION = "3.2.3.2"
-CURRENT_BUILD = "3419"
+CURRENT_BUILD = "3420"
 
-CHECK_UPDATES_INTERVAL = 60 * 60 * 1000
+CHECK_UPDATES_INTERVAL = 3 * 60 * 60 * 1000
 
 TG_HOST = "127.0.0.1"
 TG_PORT = 1443
