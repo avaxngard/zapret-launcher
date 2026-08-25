3e0053ee62af0f69debcc534ffd853af85b33507
avaxngard <trimansberg81@gmail.com>
2026-08-25 16:04:23 +0400

    Remove the unnecessary Traffic page

--- gui/pages/__init__.py
+++ gui/pages/__init__.py
@@ -9,7 +9,6 @@
 from .main_page import MainPage
 from .service_page import ServicePage
 from .lists_page import ListsPage
-from .traffic_page import TrafficPage
 from .hosts_page import HostsPage
 from .logs_page import LogsPage
 from .settings_page import SettingsPage
@@ -18,7 +17,6 @@ __all__ = [
     'MainPage',
     'ServicePage', 
     'ListsPage',
-    'TrafficPage',
     'HostsPage',
     'LogsPage',
     'SettingsPage'
