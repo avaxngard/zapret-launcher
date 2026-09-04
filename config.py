b69cd56cf6434c51b7c99151f261dd6343a47a42
avaxngard <trimansberg81@gmail.com>
2026-09-04 20:34:13 +0400

    Build number bump

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ PNG_ICON_PATH = BASE_DIR / "resources" / "icon.png"
 HOSTS_PATH = Path(r"C:\Windows\System32\drivers\etc\hosts")
 
 CURRENT_VERSION = "3.2.3.2"
-CURRENT_BUILD = "3424"
+CURRENT_BUILD = "3425"
 
 CHECK_UPDATES_INTERVAL = 3 * 60 * 60 * 1000
 
